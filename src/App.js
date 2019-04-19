679165db96a8da3fa0b390db446ffeeddd1ad1f5
Infilect Developer <dev@infilect.com>
2019-04-19 12:12:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Bye React--
           </a>
         </header>
       </div>
